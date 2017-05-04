8342cbe63b345565f2c930c6520ae722a2f50b21
bridge wang <tigerfan7495@gmail.com>
2017-05-04 11:36:46 -0400

    Update install.sh

--- box-B/install.sh
+++ box-B/install.sh
@@ -11,15 +11,14 @@ mv kumquat $DIRECTORY
 mv start_node_server.sh $DIRECTORY
 mv server.js $DIRECTORY
 
-DIRECTORY=/cm/shared/apps/u24_software/pipeline_bwang/logs
-if [ ! -d "$DIRECTORY" ]; then
-  echo "Making directory $DIRECTORY"
-  mkdir -p $DIRECTORY
-  mkdir -p $DIRECTORY/njs
-  mkdir -p $DIRECTORY/pbs
+DIRECTORY2=$DIRECTORY/logs
+if [ ! -d "$DIRECTORY2" ]; then
+  echo "Making directory $DIRECTORY2"
+  mkdir -p $DIRECTORY2
+  mkdir -p $DIRECTORY2/njs
+  mkdir -p $DIRECTORY2/pbs
 fi
 
-DIRECTORY=/cm/shared/apps/u24_software/pipeline_bwang
 cd $DIRECTORY/kumquat
 ./compile.sh
 
