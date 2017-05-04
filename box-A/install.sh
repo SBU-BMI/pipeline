7b20d691a9c8f94d74b8793af117135366bd481b
bridge wang <tigerfan7495@gmail.com>
2017-05-04 11:44:10 -0400

    Update install.sh

--- box-A/install.sh
+++ box-A/install.sh
@@ -2,7 +2,7 @@
 
 echo "MUST BE ROOT!"
 
-DIRECTORY=/usr/local/BMI/pipeline
+DIRECTORY=/usr/local/BMI/pipeline_bwang
 if [ ! -d "$DIRECTORY" ]; then
   echo "Making directory $DIRECTORY"
   mkdir -p $DIRECTORY
