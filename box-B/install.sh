d28a8fb7615136a0b7400b6a493b94d027305ca5
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-05-16 14:44:06 -0400

    changed variable name
    We had a variable name clash (software_dir assigned twice)

--- box-B/install.sh
+++ box-B/install.sh
@@ -11,15 +11,15 @@ mv kumquat $software_dir
 mv start_node_server.sh $software_dir
 mv server.js $software_dir
 
-software_dir=/cm/shared/apps/u24_software/pipeline/logs
-if [ ! -d "$software_dir" ]; then
-  echo "Making software_dir $software_dir"
-  mkdir -p $software_dir
-  mkdir -p $software_dir/njs
-  mkdir -p $software_dir/pbs
+logs_dir=$software_dir/logs
+if [ ! -d "$logs_dir" ]; then
+  echo "Making logs_dir $logs_dir"
+  mkdir -p $logs_dir
+  mkdir -p $logs_dir/njs # nodejs logs
+  mkdir -p $logs_dir/pbs # torque logs
 fi
 
-software_dir=/cm/shared/apps/u24_software/pipeline
+# Compile it
 cd $software_dir/kumquat
 ./compile.sh
 
